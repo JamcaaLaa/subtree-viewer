a25ca43fa0f5cf21415968c88ce8299ea84147f7
Gypsophila <onsummer@foxmail.com>
2021-12-24 09:36:33 +0800

    [add] gitpage config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/docs',
+  build: {
+    sourcemap: true,
+    emptyOutDir: true,
+    outDir: './docs'
+  },
   css: {
     modules: {
       localsConvention: 'camelCaseOnly'
